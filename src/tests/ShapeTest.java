cf046a29b5505aad7ccdde2336994fb79e55b92d
ajf0020 <ajf0020@csee137aer21.lcsee.wvu.edu>
2017-10-11 19:14:03 -0400

    Completed Shape Test

--- src/tests/ShapeTest.java
+++ src/tests/ShapeTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import shapes.Circle;
+
 public class ShapeTest {
 
 	/*
@@ -12,9 +14,24 @@ public class ShapeTest {
 	 * Also create SquareTest.java and CircleTest.java JUnit test cases.
 	 * Test each class's constructor method and calculateArea method.
 	 */
+	private final double DELTA = 0.0000000000001;
+	
 	@Test
-	public void testCompareTo() {
+	public void testCompareTo()
+	{
+		Circle a = new Circle(1);
+		Circle b = new Circle(1);
+		assertEquals(0, a.compareTo(b), DELTA);
 		fail("Not yet implemented");
+		
+		
+		Circle c = new Circle(100);
+		Circle d = new Circle(1);
+		assertEquals(1, c.compareTo(d), DELTA);
+		fail("Not yet implemented");
+		
 	}
+	
+	
 
 }
