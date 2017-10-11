3e9f3a4f35a84c8c46e316341de6df3103c569ba
ajf0020 <ajf0020@csee137aer21.lcsee.wvu.edu>
2017-10-11 19:11:07 -0400

    Completed Square class

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,14 +1,20 @@
 package shapes;
 
-public class Square extends Shape {
+public class Square extends Shape
+{
 	private double side;
 
 	/*
 	 * Complete the implementation of this class by finishing the constructor
 	 * below.  Squares must have a positive side length.
 	 */
-	public Square(double side) throws IllegalArgumentException{
-		// TODO Implement me
+	public Square(double side) throws IllegalArgumentException
+	{
+		if(side>0)
+		{
+			this.side = side;
+		}
+		else throw new IllegalArgumentException("The side must be greater than 0.");
 	}
 	
 	/*
@@ -17,5 +23,12 @@ public class Square extends Shape {
 
 	public double getSide() {
 		return side;
+	}
+
+	public double calculateArea() 
+	{
+		double area;
+		area = side*side;
+		return area;
 	}
 }
